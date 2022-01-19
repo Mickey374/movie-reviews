6a5eee0db723b8f0c94b38e190d1ddfc0359ae2f
Mickey374 <asantemichael37483@gmail.com>
2022-01-19 22:43:47 +0100

    Created UseState and UseEffect to consume MovieDataService

--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import MovieDataService from '../services/movies'
+import {Link} from "react-router-dom";
+import {Form, Button, Col, Row, Container} from 'react-bootstrap'
 
-function MoviesList() {
-    return (
-        <div className="App">
-            <h1>Movies List</h1>
-        </div>
-    );
+// function MoviesList() {
+//     return (
+//         <div className="App">
+            
+//         </div>
+//     );
+// }
+
+const MoviesList = (props) => {
+    const [movies, setMovies] = useState([])
+    const [searchTitle, setSearchTitle] = useState('')
+    const [searchRating, setSearchRating] = useState('')
+    const [ratings, setRatings] = useState(["All Ratings"])
+
+    useEffect(() => {
+        retrieveMovies()
+        retrieveRatings()
+    }, [])
+
+    const retrieveMovies = () => {
+        MovieDataService.getAll()
+        .then(response=>{
+            console.log(response.data);
+            setMovies(response.data.movies)
+        })
+        .catch(e=>{
+            console.log(e)
+        })
+    }
+    const retrieveRatings = () => {
+        MovieDataService.getRatings()
+        .then(response=>{
+            console.log(response.data);
+            setRatings(["All Ratings"].concat(response.data))
+        })
+        .catch(e=>{
+            console.log(e);
+        })
+    }
+
+    const onChangeSearchTitle = e =>{
+        const searchTitle = e.target.value
+        setSearchTitle(searchTitle)
+    }
+
+    const onChangeSearchRating = e =>{
+        const searchRating = e.target.value
+        setSearchRating(searchRating)
+    }
 }
 
 export default MoviesList;
