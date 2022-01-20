a1bbd15053178d5dbbe804fa3c6070c264ae3f67
Mickey374 <asantemichael37483@gmail.com>
2022-01-20 19:13:05 +0100

    Re-edited props and Refactored code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Switch, Route, Link} from "react-router-dom"
-import {Nav,Navbar} from 'react-bootstrap'
+import {Nav,Navbar, Container} from 'react-bootstrap'
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,39 +20,45 @@ function App() {
   async function logout() {
     setUser(null)
   }
+  
   return (
-    <div className="App">
+    <div className="App"> 
+    <Container>
       <Navbar bg="light" expand="lg">
         <Navbar.Brand>Movie Reviews</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link>
-                <Link to={"/movies"}>Movies</Link>
-              </Nav.Link>
-
-              <Nav.Link>
-                { user ? 
-                (<a>Logout User</a>) : 
-                (<Link to={"/login"}>Login</Link>)}
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-      </Navbar>
-
-      < Switch >
-        <Route exact path = {["/", "/movies"]} component = {MoviesList}>
-        </Route> 
-
-        <Route path = "/movies/:id/review" render = {(props) =><AddReview {...props} user = {user}/>} >
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">            
+            <Nav.Link>
+              <Link to={"/movies"}>Movies</Link>
+            </Nav.Link>            
+            <Nav.Link>
+              { user ? (
+                <Link onClick={logout}>Logout User</Link>
+              ) : (
+                <Link to={"/login"}>Login</Link>
+              )}              
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>  
+      
+      <Switch> 
+        <Route exact path={["/", "/movies"]} component={MoviesList}></Route>
+        <Route path="/movies/:id/review" render={(props)=> 
+          <AddReview {...props} user={user} />
+        }>
         </Route>
-
-        <Route path = "/movies/:id/" render = {(props) =><Movie {...props}user = {user}/>} >
+        <Route path="/movies/:id/" render={(props)=> 
+          <Movie {...props} user={user} />
+        }>
         </Route>
-
-        <Route path = "/login" render = {(props) =><Login {...props}login = {login}/>} >
-        </Route> 
-      </Switch>
+        <Route path="/login" render={(props)=> 
+          <Login {...props} login={login} />
+        }>            
+        </Route>          
+      </Switch>  
+      </Container>         
     </div>
   );
 }
--- frontend/src/components/movies-list.js
+++ frontend/src/components/movies-list.js
@@ -1,17 +1,10 @@
 import React, {useState, useEffect} from "react";
-import MovieDataService from '../services/movies'
 import {Link} from "react-router-dom";
 import {Form, Button, Col, Row, Container, Card} from 'react-bootstrap'
+import MovieDataService from '../services/movies';
 
-// function MoviesList() {
-//     return (
-//         <div className="App">
-            
-//         </div>
-//     );
-// }
 
-const MoviesList = (props) => {
+const MoviesList = props => {
     const [movies, setMovies] = useState([])
     const [searchTitle, setSearchTitle] = useState('')
     const [searchRating, setSearchRating] = useState('')
@@ -53,72 +46,99 @@ const MoviesList = (props) => {
         setSearchRating(searchRating)
     }
 
-    // <--Form to Render Data from Database--->
-    return(
-        <div className="App">
-            <Container>
-                <Form>
-                    <Row>
-                        <Col>
-                        <Form.Group>
-                            <Form.Control 
-                            type="text" 
-                            placeholder="Search by Title" 
-                            value={searchTitle}
-                            onChange={onChangeSearchTitle}
-                            />
-                        </Form.Group>
-                        <Button 
-                        variant="primary"
-                        type="button"
-                        onClick={findByTitle}
-                        >
-                        Search
-                        </Button>
-                        </Col>
-                        <Col>
-                        <Form.Group>
-                            <Form.Control 
-                            as="select"
-                            onChange={onChangeSearchRating}>
-                                {ratings.map(rating=>{
-                                    return(<option value={rating}>{rating}</option>
-                                        )
-                                })}
-                            </Form.Control>
-                        </Form.Group>
-                        <Button 
-                        variant="primary"
-                        type="button"
-                        onClick={findByRating}
-                        >
-                        Search
-                        </Button>
-                        </Col>
-                        </Row>
-                </Form>
-            
+    const find = (query, by)=>{
+        MovieDataService.find(query, by)
+        .then(response=>{
+            console.log(response.data);
+            setMovies(response.data.movies)
+        })
+        .catch(e=>{
+            console.log(e);
+        })
+    }
 
-                <Row>
-                    {movies.map((movie)=>{
-                        return (
-                            <Col>
-                                <Card style={{width: '18rem'}}>
-                                    <Card.Img src={movie.poster+"/100px180"} />
-                                    <Card.Body>
-                                        <Card.Title>{movie.title}</Card.Title>
-                                        <Card.Text>Rating: {movie.rated}</Card.Text>
-                                        <Card.Text>{movie.plot}</Card.Text>
-                                        <Link to={"/movies/"+movie._id}>View Reviews</Link>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
+    const findByTitle = ()=>{
+        find(searchTitle, "title")
+    }
+
+    const findByRating= ()=>{
+        if(searchRating === 'All Ratings'){
+            retrieveMovies()
+        }
+        else{
+            find(searchRating, "rated")
+        }
+    }
+
+    // <--Form to Render Data from Database--->
+    return (
+        <div className="App">   
+          <Container>
+            <Form>        
+              <Row>
+                <Col>
+                  <Form.Group>          
+                    <Form.Control 
+                      type="text" 
+                      placeholder="Search by title" 
+                      value={searchTitle} 
+                      onChange={onChangeSearchTitle} 
+                    />
+                  </Form.Group>        
+                  <Button 
+                    variant="primary" 
+                    type="button" 
+                    onClick={findByTitle}
+                  > 
+                    Search         
+                  </Button>
+                </Col>
+                <Col>
+                  <Form.Group>
+                    <Form.Control as="select" onChange={onChangeSearchRating} >
+                      {ratings.map(rating =>{
+                        return(
+                          <option value={rating}>{rating}</option>
                         )
-                    })}
-                </Row>
-            </Container>
+                      })}
+                    </Form.Control>       
+                  </Form.Group>    
+                  <Button 
+                      variant="primary" 
+                      type="button" 
+                      onClick={findByRating}
+                  > 
+                    Search         
+                  </Button>  
+                </Col>
+              </Row>              
+            </Form>  
+            
+              <Row>
+                {movies.map((movie) =>{
+                  return(
+                    <Col>
+                      <Card style={{ width: '18rem' }}>
+                        <Card.Img src={movie.poster+"/100px180"} />
+                        <Card.Body>
+                          <Card.Title>{movie.title}</Card.Title>
+                          <Card.Text>
+                            Rating: {movie.rated}                        
+                          </Card.Text>
+                          <Card.Text>
+                            {movie.plot}                        
+                          </Card.Text>                                            
+                          <Link to={"/movies/"+movie._id} >View Reviews</Link>
+                        </Card.Body>
+                      </Card>            
+                    </Col>                
+                  )
+                })}
+              </Row>          
+            
+          </Container>        
         </div>
-    );
+      );
 }
 
 export default MoviesList;
