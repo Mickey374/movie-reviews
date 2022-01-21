713bc8dfc7e91ed2a9f8e175aba865d1cf7c0240
Mickey374 <asantemichael37483@gmail.com>
2022-01-21 13:37:26 +0100

    Code Refactoring for movies item review

--- frontend/src/components/movie.js
+++ frontend/src/components/movie.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MovieDataService from '../services/movies'
+import {Link} from 'react-router-dom'
+import {Media,Form, Button, Col, Row, Container,Image, Card} from 'react-bootstrap'
 
-function Movie() {
+const Movie = props =>{
+    const[movie, setMovie] = useState({
+        id: null,
+        title: "",
+        rated: "",
+        reviews: []
+    })
+
+    const getMovie = id =>{
+        MovieDataService.get(id)
+        .then(response =>{
+            setMovie(response.data)
+            console.log(response.data);
+        })
+        .catch(e=>{
+            console.log(e);
+        })
+    }
+
+    useEffect(()=>{
+        getMovie(props.match.params.id)
+    }, [props.match.params.id])
+
     return (
-        <div className="App">
-            Movie
+        <div>
+            <Container>
+                <Row>
+                    <Col>
+                        <Image src={movie.poster+"/100px180"} fluid />
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Header as="h5">
+                                {movie.title}
+                            </Card.Header>
+                            <Card.Body>
+                                <Card.Text>
+                                    {movie.plot}
+                                </Card.Text>
+                                {props.user && <Link to={"/movies/"+props.match.params.id+ "/review"}>Add Review</Link>}
+                            </Card.Body>
+                        </Card>
+                        <br></br>
+                        <h2>Reviews</h2>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
